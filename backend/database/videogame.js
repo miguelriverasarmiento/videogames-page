95d0ea31d117dca18c666e51b550beacb52a507a
mgars90 <miguelriverasarmiento>
2026-04-10 15:13:41 -0500

    Agrego codigo try catch en la carpeta database

--- backend/database/videogame.js
+++ backend/database/videogame.js
@@ -2,33 +2,82 @@ const DB = require("./data.json");
 const { saveToDatabase } = require("./utils");
 
 const getAllVideogames = () => {
-  return DB.videogames;
+  try {
+    const videogames = DB.videogames;
+    if (!videogames) {
+      throw new Error("No se encontraron videojuegos en la base de datos.");
+    }
+    return videogames;
+  } catch (error) {
+    throw {
+      status: 500,
+      message: "Error fetching videogames: " + error.message,
+    };
+  }
 };
 
 const getVideogameById = (id) => {
-  return DB.videogames.find((v) => v.id == id);
+  try {
+    const videogame = DB.videogames.find((v) => v.id == id);
+    if (!videogame) {
+      throw new Error(`No se encontró un videojuego con el ID: ${id}`);
+    }
+    return videogame;
+  } catch (error) {
+    throw { status: error?.status || 500, message: error?.message || error };
+  }
 };
 
 const addVideogame = (videogame) => {
-  DB.videogames.push(videogame);
-  saveToDatabase(DB);
-  return videogame;
+  try {
+    const vExist =
+      DB.videogames.findIndex((v) => v.name === videogame.name) > -1;
+    if (vExist) {
+      throw {
+        status: 400,
+        message: `El videojuego con el nombre "${videogame.name}" ya existe.`,
+      };
+    }
+    DB.videogames.push(videogame);
+    saveToDatabase(DB);
+    return videogame;
+  } catch (error) {
+    throw { status: error?.status || 500, message: error?.message || error };
+  }
 };
 
 const updateVideogame = (id, updatedData) => {
-  const index = DB.videogames.findIndex((v) => v.id == id);
-  if (index === -1) return null;
-  DB.videogames[index] = { ...DB.videogames[index], ...updatedData };
-  saveToDatabase(DB);
-  return DB.videogames[index];
+  try {
+    const index = DB.videogames.findIndex((v) => v.id == id);
+    if (index === -1) {
+      throw {
+        status: 400,
+        message: `Videojuego con ID ${id} no encontrado.`,
+      };
+    }
+    DB.videogames[index] = { ...DB.videogames[index], ...updatedData };
+    saveToDatabase(DB);
+    return DB.videogames[index];
+  } catch (error) {
+    throw { status: error?.status || 500, message: error?.message || error };
+  }
 };
 
 const deleteVideogame = (id) => {
-  const index = DB.videogames.findIndex((v) => v.id == id);
-  if (index === -1) return null;
-  const deletedVideogame = DB.videogames.splice(index, 1); // Elimina el videojuego y lo devuelve en un array
-  saveToDatabase(DB);
-  return deletedVideogame[0];
+  try {
+    const index = DB.videogames.findIndex((v) => v.id == id);
+    if (index === -1) {
+      throw {
+        status: 400,
+        message: `Videojuego con ID ${id} no encontrado.`,
+      };
+    }
+    const deletedVideogame = DB.videogames.splice(index, 1); // Elimina el videojuego y lo devuelve en un array
+    saveToDatabase(DB);
+    return deletedVideogame[0];
+  } catch (error) {
+    throw { status: error?.status || 500, message: error?.message || error };
+  }
 };
 
 module.exports = {
