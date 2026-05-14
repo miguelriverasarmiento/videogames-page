76abef498fa0ba86f170fed929345d871bf0d98b
mgars90 <miguelriverasarmiento>
2026-05-14 16:20:45 -0500

    Agrego modificacion y actualizacion del modelo en backend

--- backend/models/Videogame.js
+++ backend/models/Videogame.js
@@ -1,12 +1,24 @@
 const mongoose = require("mongoose");
 
-const videogameSchema = new mongoose.Schema({
-  titulo: { type: String, required: true },
-  imagen: { type: String },
-  descripcion: { type: String },
-  genero: { type: String, required: true },
-  plataforma: { type: String, required: true },
-  precio: { type: Number, required: true },
-});
+const videogameSchema = new mongoose.Schema(
+  {
+    titulo: { type: String, required: true },
+    imagen: { type: String },
+    descripcion: { type: String },
+    genero: { type: String, required: true },
+    plataforma: { type: String, required: true },
+    precio: { type: Number, required: true },
+  },
+  {
+    toJSON: {
+      transform: (doc, ret) => {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+        return ret;
+      },
+    },
+  },
+);
 
 module.exports = mongoose.model("Videogame", videogameSchema);
